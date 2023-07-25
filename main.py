bfdf5b94835c2ae1670c562ba8093692c92fadb8
brunodos-santos <bruno6.santos@usp.br>
2023-07-25 16:13:58 -0300

    Criação do movimento e da mudança de ângulo que da bola ao bater na raquete ou nas bordas.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+from time import sleep
 
 screen = Screen()
 screen.bgcolor("black")
@@ -9,7 +11,7 @@ screen.tracer(0)
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
-
+ball = Ball()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -19,6 +21,18 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_on = True
 while game_on:
+    sleep(0.1)
     screen.update()
+    ball.move()
+
+    # Detect collision with wall.
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # Detect ball collision with r_paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320:
+        ball.bounce_x()
+    elif ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
 
 screen.exitonclick()
